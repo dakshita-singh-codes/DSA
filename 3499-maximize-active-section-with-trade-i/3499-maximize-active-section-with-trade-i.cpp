37e15f507994b48e8b7ad47376aafa95f90ff10d
Dakshita Singh <dakshita.singh2307@gmail.com>
2026-07-21 23:10:27 +0530

    Time: 101 ms (30.63%), Space: 80.8 MB (36.04%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n=s.length();
+        int activeCount= count(begin(s),end(s),'1');
+        vector<int> inactiveBlocks;
+        int i=0;
+        while(i<n){
+            if(s[i]=='0'){
+                int start=i;
+                while(i<n && s[i]=='0') i++;
+
+                inactiveBlocks.push_back(i-start);
+
+            }else{
+                i++;
+            }
+        }
+        int maxPairsum=0;
+        for(int i=1;i<inactiveBlocks.size();i++){
+            maxPairsum=max(maxPairsum,inactiveBlocks[i]+inactiveBlocks[i-1]);
+        }
+        return maxPairsum+activeCount;
+    }
+};
