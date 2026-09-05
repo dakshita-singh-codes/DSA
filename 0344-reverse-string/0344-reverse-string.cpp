bc7cf78f9491569334ef398dfa1912e310561b60
Dakshita Singh <dakshita.singh2307@gmail.com>
2026-09-05 22:42:13 +0530

    Time: 0 ms (100%), Space: 27.2 MB (80.92%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    void reverseString(vector<char>& s) {
+        reverse(s,0,s.size());
+    }
+    void reverse(vector<char>& s,int i,int n){
+        if(i>=n/2){
+            return;
+        }
+        swap(s[i],s[n-i-1]);
+        reverse(s,i+1,n);
+    }
+};
